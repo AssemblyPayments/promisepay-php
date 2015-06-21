0cf97f604a0625455b58c8032ed4fa1b09cb8a10
Mihail Mironov <m.mironov94@gmail.com>
2015-06-21 14:16:43 +0300

    delite, getvyID user test

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -45,8 +45,7 @@ class UserTest extends \PHPUnit_Framework_TestCase {
         \PHPUnit_Framework_Error_Notice::$enabled = FALSE;
         $repo = new UserRepository();
         $id = GUID();
-        //$user = new User();
-        //$user->setId();
+
         $arr = array(
             "id"            => $id,
             "first_name"    => 'Test',
@@ -63,7 +62,7 @@ class UserTest extends \PHPUnit_Framework_TestCase {
 
         $user = new User($arr);
 
-        //$user = json_decode(json_encode($arr), FALSE);
+
 
         $createdUser = $repo->createUser($user);
 
@@ -81,6 +80,205 @@ class UserTest extends \PHPUnit_Framework_TestCase {
     }
 
 
+
+
+    /**
+     * @expectedException PromisePay\Exception\Validation
+     */
+    public function CreateUserIdMissing()
+    {
+        \PHPUnit_Framework_Error_Notice::$enabled = FALSE;
+        $repo = new UserRepository();
+        $id = GUID();
+
+        $arr = array(
+            "id"            => '',
+            "first_name"    => 'Test',
+            "last_name"     => 'Test',
+            "email"         => $id . '@google.com',
+            "mobile"        => $id.'3213125551223',
+            "address_line1" => 'a line 1',
+            "address_line2" => 'a line 2',
+            "state"         => 'state',
+            "city"          => 'city',
+            "zip"           => '90210',
+            "country"       => 'AUS',
+        );
+
+        $user = new User($arr);
+        $repo->createUser($user);
+    }
+
+
+
+    /**
+     * @expectedException PromisePay\Exception\Validation
+     */
+    public function createUserMissedFirstname()
+    {
+        \PHPUnit_Framework_Error_Notice::$enabled = FALSE;
+        $repo = new UserRepository();
+        $id = GUID();
+
+        $arr = array(
+            "id"            => $id,
+            "first_name"    => '',
+            "last_name"     => 'Test',
+            "email"         => $id . '@google.com',
+            "mobile"        => $id.'3213125551223',
+            "address_line1" => 'a line 1',
+            "address_line2" => 'a line 2',
+            "state"         => 'state',
+            "city"          => 'city',
+            "zip"           => '90210',
+            "country"       => 'AUS',
+        );
+        $user = new User($arr);
+        $repo->createUser($user);
+    }
+
+
+    /**
+     * @expectedException PromisePay\Exception\Validation
+     */
+    public function createUserWrongCountry()
+    {
+        \PHPUnit_Framework_Error_Notice::$enabled = FALSE;
+        $repo = new UserRepository();
+        $id = GUID();
+
+        $arr = array(
+            "id"            => $id,
+            "first_name"    => 'test',
+            "last_name"     => 'Test',
+            "email"         => $id . '@google.com',
+            "mobile"        => $id.'3213125551223',
+            "address_line1" => 'a line 1',
+            "address_line2" => 'a line 2',
+            "state"         => 'state',
+            "city"          => 'city',
+            "zip"           => '90210',
+            "country"       => 'AUSee',
+        );
+        $user = new User($arr);
+        $repo->createUser($user);
+    }
+
+
+    /**
+     * @expectedException PromisePay\Exception\Validation
+     */
+    public function createUserWrongEmail()
+    {
+        \PHPUnit_Framework_Error_Notice::$enabled = FALSE;
+        $repo = new UserRepository();
+        $id = GUID();
+
+        $arr = array(
+            "id"            => $id,
+            "first_name"    => 'Tese',
+            "last_name"     => 'Test',
+            "email"         => $id . 'google.com',
+            "mobile"        => $id.'3213125551223',
+            "address_line1" => 'a line 1',
+            "address_line2" => 'a line 2',
+            "state"         => 'state',
+            "city"          => 'city',
+            "zip"           => '90210',
+            "country"       => 'AUS',
+        );
+        $user = new User($arr);
+        $repo->createUser($user);
+    }
+
+
+    public function getUserSuccess()
+    {
+        \PHPUnit_Framework_Error_Notice::$enabled = FALSE;
+        $repo = new UserRepository();
+        $id = GUID();
+
+        $arr = array(
+            "id"            => $id,
+            "first_name"    => 'test',
+            "last_name"     => 'Test',
+            "email"         => $id . '@google.com',
+            "mobile"        => $id.'3213125551223',
+            "address_line1" => 'a line 1',
+            "address_line2" => 'a line 2',
+            "state"         => 'state',
+            "city"          => 'city',
+            "zip"           => '90210',
+            "country"       => 'AUS',
+        );
+        $user = new User($arr);
+        $createdUser = $repo->createUser($user);
+
+        $findUser = $repo->getUserById($id);
+        $this->assertEquals($createdUser->id, $findUser->getId());
+    }
+
+    /**
+     * @expectedException PromisePay\Exception\Argument
+     */
+    public function getUserMissedID()
+    {
+
+        \PHPUnit_Framework_Error_Notice::$enabled = FALSE;
+        $repo = new UserRepository();
+        $id = GUID();
+        $arr = array(
+            "id"            => $id,
+            "first_name"    => 'test',
+            "last_name"     => 'Test',
+            "email"         => $id . '@google.com',
+            "mobile"        => $id.'3213125551223',
+            "address_line1" => 'a line 1',
+            "address_line2" => 'a line 2',
+            "state"         => 'state',
+            "city"          => 'city',
+            "zip"           => '90210',
+            "country"       => 'AUS',
+        );
+        $user = new User($arr);
+        $repo->createUser($user);
+        $missed = '';
+        $repo->getUserById($missed);
+    }
+
+
+
+    public function deleteUserbyIdsuccessful()
+    {
+        \PHPUnit_Framework_Error_Notice::$enabled = FALSE;
+        $repo = new UserRepository();
+        $repo->deleteUser('d8cc55d2-a5ab-476d-9816-9d7491d522c5');
+        $repo->getUserById('d8cc55d2-a5ab-476d-9816-9d7491d522c5');
+    }
+
+    /**
+     * @expectedException PromisePay\Exception\Argument
+     */
+    public function deleteUserbyIdmissedId()
+    {
+        \PHPUnit_Framework_Error_Notice::$enabled = FALSE;
+        $repo = new UserRepository();
+        $repo->deleteUser('');
+
+    }
+
+
+// edit user success
+// edit user missed id
+
+
+// send mibilepin successful
+
+// list user items successful
+// list user bank accounts
+// list user CardAccounts
+// list user Ppal accounts
+//list user disbursement accounts successful
 
 }
  

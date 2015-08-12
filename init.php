fe6b0c86e03c4041fd0af10744caca5feb484b84
Mihail Mironov <m.mironov94@gmail.com>
2015-08-12 14:39:07 +0300

    dirname constant fix

--- init.php
+++ init.php
@@ -1,65 +1,66 @@
 <?php
 //error_reporting(0);
 //configuration
-require(__DIR__  . '/lib/Configuration.php');
+require(dirname(__FILE__) . '/lib/Configuration.php');
 //repos
-require(__DIR__ . '/lib/ApiAbstract.php');
-require(__DIR__  . '/lib/AddressRepository.php');
-require(__DIR__  . '/lib/BankAccountRepository.php');
-require(__DIR__  . '/lib/CardAccountRepository.php');
-require(__DIR__  . '/lib/CompanyRepository.php');
-require(__DIR__  . '/lib/FeeRepository.php');
-require(__DIR__  . '/lib/ItemRepository.php');
-require(__DIR__  . '/lib/PayPalAccountRepository.php');
-require(__DIR__  . '/lib/TokenRepository.php');
-require(__DIR__  . '/lib/TransactionRepository.php');
-require(__DIR__  . '/lib/UploadRepository.php');
-require(__DIR__  . '/lib/UserRepository.php');
+require(dirname(__FILE__) . '/lib/ApiAbstract.php');
+require(dirname(__FILE__) . '/lib/AddressRepository.php');
+require(dirname(__FILE__) . '/lib/BankAccountRepository.php');
+require(dirname(__FILE__) . '/lib/CardAccountRepository.php');
+require(dirname(__FILE__) . '/lib/CompanyRepository.php');
+require(dirname(__FILE__) . '/lib/FeeRepository.php');
+require(dirname(__FILE__) . '/lib/ItemRepository.php');
+require(dirname(__FILE__) . '/lib/PayPalAccountRepository.php');
+require(dirname(__FILE__) . '/lib/TokenRepository.php');
+require(dirname(__FILE__) . '/lib/TransactionRepository.php');
+require(dirname(__FILE__) . '/lib/UploadRepository.php');
+require(dirname(__FILE__) . '/lib/UserRepository.php');
 
 //data_objectslist
-require(__DIR__  . '/lib/DataObjects/Object.php');
-require(__DIR__  . '/lib/DataObjects/AccountAbstract.php');
-require(__DIR__  . '/lib/DataObjects/Address.php');
-require(__DIR__  . '/lib/DataObjects/Bank.php');
-require(__DIR__  . '/lib/DataObjects/BankAccount.php');
-require(__DIR__  . '/lib/DataObjects/BPayDetails.php');
-require(__DIR__  . '/lib/DataObjects/Card.php');
-require(__DIR__  . '/lib/DataObjects/CardAccount.php');
-require(__DIR__  . '/lib/DataObjects/Company.php');
-require(__DIR__  . '/lib/DataObjects/DetailsContainer.php');
-require(__DIR__  . '/lib/DataObjects/DisbursementAccount.php');
-require(__DIR__  . '/lib/DataObjects/Errors.php');
-require(__DIR__  . '/lib/DataObjects/Fee.php');
-require(__DIR__  . '/lib/DataObjects/Item.php');
-require(__DIR__  . '/lib/DataObjects/ItemStatus.php');
-require(__DIR__  . '/lib/DataObjects/PayPal.php');
-require(__DIR__  . '/lib/DataObjects/PayPalAccount.php');
-require(__DIR__  . '/lib/DataObjects/Token.php');
-require(__DIR__  . '/lib/DataObjects/Transaction.php');
-require(__DIR__  . '/lib/DataObjects/Upload.php');
-require(__DIR__  . '/lib/DataObjects/User.php');
-require(__DIR__  . '/lib/DataObjects/Widget.php');
-require(__DIR__  . '/lib/DataObjects/WireDetails.php');
+require(dirname(__FILE__) . '/lib/DataObjects/Object.php');
+require(dirname(__FILE__) . '/lib/DataObjects/AccountAbstract.php');
+require(dirname(__FILE__) . '/lib/DataObjects/Address.php');
+require(dirname(__FILE__) . '/lib/DataObjects/Bank.php');
+require(dirname(__FILE__) . '/lib/DataObjects/BankAccount.php');
+require(dirname(__FILE__) . '/lib/DataObjects/BPayDetails.php');
+require(dirname(__FILE__) . '/lib/DataObjects/Card.php');
+require(dirname(__FILE__) . '/lib/DataObjects/CardAccount.php');
+require(dirname(__FILE__) . '/lib/DataObjects/Company.php');
+require(dirname(__FILE__) . '/lib/DataObjects/DetailsContainer.php');
+require(dirname(__FILE__) . '/lib/DataObjects/DisbursementAccount.php');
+require(dirname(__FILE__) . '/lib/DataObjects/Errors.php');
+require(dirname(__FILE__) . '/lib/DataObjects/Fee.php');
+require(dirname(__FILE__) . '/lib/DataObjects/Item.php');
+require(dirname(__FILE__) . '/lib/DataObjects/ItemStatus.php');
+require(dirname(__FILE__) . '/lib/DataObjects/PayPal.php');
+require(dirname(__FILE__) . '/lib/DataObjects/PayPalAccount.php');
+require(dirname(__FILE__) . '/lib/DataObjects/Token.php');
+require(dirname(__FILE__) . '/lib/DataObjects/Transaction.php');
+require(dirname(__FILE__) . '/lib/DataObjects/Upload.php');
+require(dirname(__FILE__) . '/lib/DataObjects/User.php');
+require(dirname(__FILE__) . '/lib/DataObjects/Widget.php');
+require(dirname(__FILE__) . '/lib/DataObjects/WireDetails.php');
 
 //Enums
-require(__DIR__  . '/lib/Enum/FeeType.php');
-require(__DIR__  . '/lib/Enum/PaymentType.php');
+require(dirname(__FILE__) . '/lib/Enum/FeeType.php');
+require(dirname(__FILE__) . '/lib/Enum/PaymentType.php');
 
 //Exception
-require(__DIR__  . '/lib/Exception/Base.php');
-require(__DIR__  . '/lib/Exception/Api.php');
-require(__DIR__  . '/lib/Exception/Argument.php');
-require(__DIR__  . '/lib/Exception/Credentials.php');
-require(__DIR__  . '/lib/Exception/Misconfiguration.php');
-require(__DIR__  . '/lib/Exception/NotFound.php');
-require(__DIR__  . '/lib/Exception/Unathorized.php');
-require(__DIR__  . '/lib/Exception/Validation.php');
+require(dirname(__FILE__) . '/lib/Exception/Base.php');
+require(dirname(__FILE__) . '/lib/Exception/Api.php');
+require(dirname(__FILE__) . '/lib/Exception/Argument.php');
+require(dirname(__FILE__) . '/lib/Exception/Credentials.php');
+require(dirname(__FILE__) . '/lib/Exception/Misconfiguration.php');
+require(dirname(__FILE__) . '/lib/Exception/NotFound.php');
+require(dirname(__FILE__) . '/lib/Exception/Unathorized.php');
+require(dirname(__FILE__) . '/lib/Exception/Validation.php');
 
 //custom Logger
-require(__DIR__  . '/lib/Log/Logger.php');
+require(dirname(__FILE__) . '/lib/Log/Logger.php');
 
 //Rest
-require(__DIR__  . '/lib/Vendors/Httpful/Bootstrap.php');
-require(__DIR__  . '/lib/Vendors/Httpful/Http.php');
-require(__DIR__  . '/lib/Vendors/Httpful/Request.php');
+require(dirname(__FILE__) . '/lib/Vendors/Httpful/Bootstrap.php');
+require(dirname(__FILE__) . '/lib/Vendors/Httpful/Http.php');
+require(dirname(__FILE__) . '/lib/Vendors/Httpful/Request.php');
 
+//require(dirname(__FILE__) . '/lib/Constructor.php');

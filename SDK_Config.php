de54965dcc40cf5aaaaa334c0e84373ee20a9794
Nino Škopac <nino.skopac@gmail.com>
2015-10-27 18:01:04 +0100

    Added API_URL Constant
    I don't think there's need for API_KEY constant as well, since we can
    easily calculate that on our end.

--- SDK_Config.php
+++ SDK_Config.php
@@ -3,4 +3,10 @@ namespace PromisePay;
 
 define('USERNAME', 'john.smith@promisepay.com');
 define('PASSWORD', '8479c2e6a1d4aeac0b3443beb7564f15');
+
+/*
+ * Test environment:        https://test.api.promisepay.com/
+ * Production environment:  https://secure.api.promisepay.com/
+*/
+define('API_URL', 'https://test.api.promisepay.com/');
 ?>

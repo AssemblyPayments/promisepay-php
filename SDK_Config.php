4f8754f6cfa6aa693c691d2f807151ffc65208f7
Nino Škopac <nino.skopac@gmail.com>
2015-10-27 18:06:48 +0100

    Slight changes

--- SDK_Config.php
+++ SDK_Config.php
@@ -5,6 +5,8 @@ define('USERNAME', 'john.smith@promisepay.com');
 define('PASSWORD', '8479c2e6a1d4aeac0b3443beb7564f15');
 
 /*
+ * SUPPORTED ENVIRONMENT VALUES
+ *
  * Test environment:        https://test.api.promisepay.com/
  * Production environment:  https://secure.api.promisepay.com/
 */

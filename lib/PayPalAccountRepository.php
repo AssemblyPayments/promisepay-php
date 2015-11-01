156fc2fd0489c49e7fb33d7e540fc255d5c89955
Nino Škopac <nino.skopac@gmail.com>
2015-11-01 21:36:21 +0100

    PayPalAccountRepository comments

--- lib/PayPalAccountRepository.php
+++ lib/PayPalAccountRepository.php
@@ -7,26 +7,47 @@ use PromisePay\DataObjects\User;
 use PromisePay\Exception;
 use PromisePay\Log;
 
-class PayPalAccountRepository extends BaseRepository
-{
-    public function getPayPalAccountById($id)
-    {
+/**
+ * Class PayPalAccountRepository
+ *
+ * @package PromisePay
+ */
+class PayPalAccountRepository extends BaseRepository {
+    /**
+     * List a PayPal account on a marketplace.
+     *
+     * Quries API for PayPal account data.
+     * Input ID is not numeric, but in format of "ec9bf096-c505-4bef-87f6-18822b9dbf2c".
+     * Returns PayPalAccount object containing account data from request response. 
+     * 
+     * @param string $id
+     * @return PayPalAccount object
+     */
+    public function getPayPalAccountById($id) {
         $this->checkIdNotNull($id);
         $response = $this->RestClient('get', 'paypal_accounts/'.$id);
         $jsonData = json_decode($response->raw_body, true)['paypal_accounts'];
         $accounts = new PayPalAccount($jsonData);
         return $accounts;
     }
-
-    public function createPayPalAccount(PayPalAccount $paypal)
-    {
+    
+    /**
+     * Creates a PayPal account for a user on a marketplace.
+     *
+     * Input accepts PayPalAccount object. 
+     * Returns new PayPalAccount object containing data from request response.
+     *
+     * @param PayPalAccount $paypal
+     * @return PayPalAccount 
+     */
+    public function createPayPalAccount(PayPalAccount $paypal) {
         $payload = '';
 
         $preparePayload = array(
-            "user_id" =>$paypal->getUserId(),
-            "paypal_email"=>$paypal->getPayPal()->getPayPalAccountEmail(),
-
+            "user_id"      => $paypal->getUserId(),
+            "paypal_email" => $paypal->getPayPal()->getPayPalAccountEmail()
         );
+        
         foreach ($preparePayload as $key => $value)
         {
             $payload .= $key . '=';
@@ -38,16 +59,33 @@ class PayPalAccountRepository extends BaseRepository
         $jsonData = json_decode($response->raw_body, true)['paypal_accounts'];
         return new PayPalAccount($jsonData);
     }
-
-    public function deletePayPalAccount($id)
-    {
+    
+    /**
+     * Deletes a PayPal account for a user on a marketplace. 
+     * Sets the account to in-active.
+     *
+     * Input ID is not numeric, but in format of "ec9bf096-c505-4bef-87f6-18822b9dbf2c".
+     * Returns REST request response object.
+     *
+     * @param string $id
+     * @return object
+     */
+    public function deletePayPalAccount($id) {
         $this->checkIdNotNull($id);
         $response = $this->RestClient('delete', 'paypal_accounts/'.$id);
         return $response;
     }
-
-    public function getUserForPayPalAccount($id)
-    {
+    
+    /**
+     * Shows the user the PayPal account is associated with.
+     *
+     * Input ID is not numeric, but in format of "ec9bf096-c505-4bef-87f6-18822b9dbf2c".
+     * Returns User object containing user data.
+     *
+     * @param string $id
+     * @return User|null
+     */
+    public function getUserForPayPalAccount($id) {
         $this->checkIdNotNull($id);
         $response = $this->RestClient('get','/paypal_accounts/'.$id.'/users');
         $jsonRaw = json_decode($response->raw_body, true);

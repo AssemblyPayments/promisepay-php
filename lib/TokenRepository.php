fbd62581ca3fb2b36b63380ae3b3d4588e1a7c69
Michael Kubler <michael@sveltestudios.com>
2016-02-01 01:19:29 +0000

    Added the requestTokenAuths function to the Token Repository

--- lib/TokenRepository.php
+++ lib/TokenRepository.php
@@ -22,4 +22,33 @@ class TokenRepository
         
         return $jsonDecodedResponse;
     }
+
+
+    /**
+     * Request Token Auths
+     *
+     * == Example $params ==
+     *
+     * Card Token :
+     * [ 'token_type' => 'card', 'user_id' => $user_id ]
+     *
+     * Approve token :
+     * [ 'token_type' => 4, 'email' => $marketplace_users_email, 'password' => $marketplace_users_password ]
+     *
+     * EUI token :
+     * // Not available yet
+     *
+     * http://reference.promisepay.com/docs/generate-a-card-token
+     *
+     * @since 1.2 As per https://promisepay-api.readme.io/docs/generate-a-card-token
+     * @param  array $params
+     * @return array JSON decoded API response
+     */
+    public static function requestTokenAuths($params)
+    {
+        $response = PromisePay::RestClient('post', 'token_auths/', $params);
+        $jsonDecodedResponse = json_decode($response->raw_body, true);
+
+        return $jsonDecodedResponse;
+    }
 }

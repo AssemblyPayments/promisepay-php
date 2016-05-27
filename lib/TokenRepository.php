ae512c09b44dbd1b8cdaf814685484f0e5fb70c8
Nino <nino.skopac@gmail.com>
2016-05-28 03:38:02 +0700

    Token Auth -> Generate Card Token

--- lib/TokenRepository.php
+++ lib/TokenRepository.php
@@ -5,18 +5,21 @@ use PromisePay\Exception;
 use PromisePay\Log;
 use PromisePay\PromisePay;
 
-class TokenRepository
-{
-    public static function requestToken()
-    {
+class TokenRepository {
+    public static function generateCardToken($params) {
+        PromisePay::RestClient('post', 'token_auths/', $params);
+        
+        return PromisePay::getDecodedResponse('token_auth');
+    }
+    
+    public static function requestToken() {
         $response = PromisePay::RestClient('get', 'request_token/');
         $jsonDecodedResponse = json_decode($response->raw_body, true);
         
         return $jsonDecodedResponse;
     }
 
-    public static function requestSessionToken($params)
-    {
+    public static function requestSessionToken($params) {
         $response = PromisePay::RestClient('get', 'request_session_token/', $params);
         $jsonDecodedResponse = json_decode($response->raw_body, true);
         
--- tests/TokenTest.php
+++ tests/TokenTest.php
@@ -4,11 +4,13 @@ use PromisePay\PromisePay;
 
 class TokenTest extends \PHPUnit_Framework_TestCase {
     
-    protected $tokenData;
+    protected $tokenData, $userId;
     
     public function setUp() {
+        $this->userId = 'ec9bf096-c505-4bef-87f6-18822b9dbf2c';
+        
         $this->tokenData = array(
-            'current_user_id'    => 'ec9bf096-c505-4bef-87f6-18822b9dbf2c',
+            'current_user_id'    => $this->userId,
             'item_name'          => 'bear',
             'amount'             => '$100',
             'external_item_id'   => 'b3b4d024-a6de-4b04-9f8d-6545eef3b28f',
@@ -25,6 +27,20 @@ class TokenTest extends \PHPUnit_Framework_TestCase {
             'buyer_country'      => 'AUS',
             'external_buyer_id'  => 'fdf58725-96bd-4bf8-b5e6-9b61be20662e',
         );
+    }
+    
+    public function testGenerateCardToken() {
+        $tokenType = 'card';
+        
+        $params = array(
+            'token_type' => $tokenType,
+            'user_id' => $this->userId
+        );
+        
+        $cardToken = PromisePay::Token()->generateCardToken($params);
+        
+        $this->assertEquals($cardToken['token_type'], $tokenType);
+        $this->assertEquals($cardToken['user_id'], $this->userId);
     }
     
     /**

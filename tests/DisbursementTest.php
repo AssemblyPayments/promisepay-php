70403505ebe73e10f29337bb563fc1ffe8875665
Nino <nino.skopac@gmail.com>
2016-06-09 19:14:04 +0700

    Removed Disbursement tests

--- tests/DisbursementTest.php
+++ tests/DisbursementTest.php
@@ -10,16 +10,4 @@ class DisbursementTest extends \PHPUnit_Framework_TestCase {
     public function setUp() {
         $this->userId = 'ec9bf096-c505-4bef-87f6-18822b9dbf2c';
     }
-    
-    public function testGet() {
-        $this->markTestSkipped();
-        
-        $get = PromisePay::Disbursement()->get();
-    }
-    
-    public function testGetById() {
-        $this->markTestSkipped();
-        
-        $disbursement = PromisePay::Disbursement()->get($this->userId);
-    }
 }

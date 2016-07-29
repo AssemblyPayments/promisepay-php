50be0a38d49bfe52d3567c471ea2bac55c979de6
Nino Škopac <nino.skopac@gmail.com>
2016-07-29 21:27:53 +0200

    Removed obsolete AsyncTest->testAsyncRequestsAlternateSyntax() test

--- tests/AsyncTest.php
+++ tests/AsyncTest.php
@@ -5,8 +5,6 @@ use PromisePay\PromisePay;
 
 class AsyncTest extends \PHPUnit_Framework_TestCase {
     
-    protected $addressId = '07ed45e5-bb9d-459f-bb7b-a02ecb38f443';
-    protected $buyerId = 'ec9bf096-c505-4bef-87f6-18822b9dbf2c';
     protected $userId = 'ec9bf096-c505-4bef-87f6-18822b9dbf2c';
     
     protected $cardTokenData = array(
@@ -79,53 +77,6 @@ class AsyncTest extends \PHPUnit_Framework_TestCase {
         foreach($batchTransactions as $batchTransaction) {
             $this->assertNotNull($batchTransaction['id']);
         }
-    }
-    
-    public function testAsyncRequestsAlternateSyntax() {
-        $this->markTestSkipped();
-        
-        $transactionsList = function() {
-            return PromisePay::Transaction()->getList(
-                array(
-                    'limit' => 2,
-                    'transaction_type' => 'refund'
-                )
-            );
-        };
-        
-        $addressId = $this->addressId;
-        
-        $address = function() use ($addressId) {
-            return PromisePay::Address()->get($addressId);
-        };
-        
-        $buyerId = $this->buyerId;
-        
-        $walletAccounts = function() use ($buyerId) {
-            return PromisePay::User()->getListOfWalletAccounts($buyerId);
-        };
-        
-        $results = PromisePay::asyncRequests(
-            $transactionsList,
-            $address,
-            $walletAccounts
-        );
-        
-        // transactions tests
-        $this->assertNotNull($results['transactions']);
-        $this->assertTrue(is_array($results['transactions']));
-        $this->assertNotNull($results['transactions'][0]['id']);
-        $this->assertEquals($results['transactions'][0]['type'], 'refund');
-        
-        // address tests
-        $this->assertNotNull($results['addresses']);
-        $this->assertTrue(is_array($results['addresses']));
-        $this->assertEquals($results['addresses']['id'], $addressId);
-        
-        // wallet accounts tests
-        $this->assertNotNull($results['wallet_accounts']);
-        $this->assertTrue(is_array($results['wallet_accounts']));
-        $this->assertContains($buyerId, $results['wallet_accounts']['links']['self']);
     }
     
 }

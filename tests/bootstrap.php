e9d28f61270176a69fbe72ee591604920a179ebd
ninoskopac <nino.skopac@gmail.com>
2017-07-30 05:39:43 +0200

    Fixes failing PHP7 tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -20,7 +20,14 @@ require_once __DIR__ . '/functions.php';
 // Project files autoloader
 require __DIR__ . '/../autoload.php';
 
+// PHPUnit 6 introduced a breaking change that
+// removes PHPUnit_Framework_TestCase as a base class,
+// and replaces it with \PHPUnit\Framework\TestCase
+if (!class_exists('\PHPUnit_Framework_TestCase') && class_exists('\PHPUnit\Framework\TestCase'))
+    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
+
 // Setup testing environment
 PromisePay::Configuration()->environment('prelive');
 PromisePay::Configuration()->login('idsidorov@gmail.com');
 PromisePay::Configuration()->password('d897f812e8485728e1de7d8ae092b75a');
+

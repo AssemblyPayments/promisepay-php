569d8ba2a4ed7c0da8a47bc91f191025be3cc8b9
Aaron Florey <azza@jcaks.net>
2019-03-01 13:44:34 +1100

    Add bPay/NPP wallet endpoints

--- lib/WalletAccounts.php
+++ lib/WalletAccounts.php
@@ -1,32 +1,109 @@
 <?php
+
 namespace PromisePay;
 
-class WalletAccounts {
-    public function get($id) {
+class WalletAccounts
+{
+    /**
+     * @param $id
+     * @return array|mixed|null
+     * @throws Exception\Api
+     * @throws Exception\ApiUnsupportedRequestMethod
+     * @throws Exception\NotFound
+     * @throws Exception\Unauthorized
+     */
+    public function get($id)
+    {
         PromisePay::RestClient('get', 'wallet_accounts/' . $id);
-        
+
         return PromisePay::getDecodedResponse('wallet_accounts');
     }
-    
-    public function show($id) {
+
+    /**
+     * @param $id
+     * @return array|mixed|null
+     * @throws Exception\Api
+     * @throws Exception\ApiUnsupportedRequestMethod
+     * @throws Exception\NotFound
+     * @throws Exception\Unauthorized
+     */
+    public function show($id)
+    {
         return $this->get($id);
     }
-    
-    public function withdraw($id, $params) {
+
+    /**
+     * @param $id
+     * @param $params
+     * @return array|mixed|null
+     * @throws Exception\Api
+     * @throws Exception\ApiUnsupportedRequestMethod
+     * @throws Exception\NotFound
+     * @throws Exception\Unauthorized
+     */
+    public function withdraw($id, $params)
+    {
         PromisePay::RestClient('post', 'wallet_accounts/' . $id . '/withdraw', $params);
-        
+
         return PromisePay::getDecodedResponse('disbursements');
     }
-    
-    public function deposit($id, $params) {
+
+    /**
+     * @param $id
+     * @param $params
+     * @return array|mixed|null
+     * @throws Exception\Api
+     * @throws Exception\ApiUnsupportedRequestMethod
+     * @throws Exception\NotFound
+     * @throws Exception\Unauthorized
+     */
+    public function deposit($id, $params)
+    {
         PromisePay::RestClient('post', 'wallet_accounts/' . $id . '/deposit', $params);
-        
+
         return PromisePay::getDecodedResponse('disbursements');
     }
-    
-    public function getUser($id) {
+
+    /**
+     * @param $id
+     * @return array|mixed|null
+     * @throws Exception\Api
+     * @throws Exception\ApiUnsupportedRequestMethod
+     * @throws Exception\NotFound
+     * @throws Exception\Unauthorized
+     */
+    public function getUser($id)
+    {
         PromisePay::RestClient('get', 'wallet_accounts/' . $id . '/users');
-        
+
         return PromisePay::getDecodedResponse('users');
+    }
+
+    /**
+     * @param $id
+     * @return array|mixed|null
+     * @throws Exception\Api
+     * @throws Exception\ApiUnsupportedRequestMethod
+     * @throws Exception\NotFound
+     * @throws Exception\Unauthorized
+     */
+    public function getBpayDetails($id) {
+        PromisePay::RestClient('get', 'wallet_accounts/' . $id . '/bpay_details');
+
+        return PromisePay::getDecodedResponse('wallet_accounts');
+    }
+
+    /**
+     * @param $id
+     * @return array|mixed|null
+     * @throws Exception\Api
+     * @throws Exception\ApiUnsupportedRequestMethod
+     * @throws Exception\NotFound
+     * @throws Exception\Unauthorized
+     */
+    public function getNppDetails($id) {
+        PromisePay::RestClient('get', 'wallet_accounts/' . $id . '/npp_details');
+
+        return PromisePay::getDecodedResponse('wallet_accounts');
     }
 }
